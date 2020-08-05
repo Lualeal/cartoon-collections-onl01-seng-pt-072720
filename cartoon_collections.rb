13205c109cdfa99f640f6a35c5edc1e2a78cdf41
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 19:25:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,11 @@ def roll_call_dwarves(array)
 end 
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  elements = []
+  array.collect do |array|
+    elements << array.collect
+    return elements.upcase
 end
 
 def long_planeteer_calls# code an argument here
