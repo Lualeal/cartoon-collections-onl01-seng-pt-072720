43f513825981cd1bfaba93c97d192d1a420f2fd5
Luana Leal <luana.leal.eq@gmail.com>
2020-08-05 19:28:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,12 @@ def summon_captain_planet(array)
   elements
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  
 end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
